de49693e812fbfa710955b36ca4e3776e1b8b3a7
Mayank Soni <mayanksonnii@gmail.com>
2023-08-27 10:26:43 +0530

    added the functionality for changing the configurations for chat completions API

--- plugin/src/utils/contentScript.js
+++ plugin/src/utils/contentScript.js
@@ -1,13 +1,27 @@
-import { showSuggestions, createSuggestionBox, getConfiguration } from './utils'
-;(async () => {
+import {
+    showSuggestions,
+    createSuggestionBox,
+    getConfiguration,
+    transformText,
+    createSpinner,
+    removeSpinner,
+} from './utils'
+
+const mainScript = async () => {
     let { isEnabled, topP, temperature, nSuggestions } =
-    await getConfiguration()
-    
+        await getConfiguration()
+
     //listener for any changes in the local storage
     chrome.storage.onChanged.addListener(async () => {
-        console.log('calling the listener');
-        ({ isEnabled, topP, temperature, nSuggestions } =
-            await getConfiguration())
+        console.log('calling the listener')
+        try {
+            ;({ isEnabled, topP, temperature, nSuggestions } =
+                await getConfiguration())
+        } catch (err) {
+            console.log('error:', err)
+        }
+
+        console.log(isEnabled, nSuggestions, topP, temperature)
     })
 
     const interval = setInterval(() => {
@@ -18,40 +32,79 @@ import { showSuggestions, createSuggestionBox, getConfiguration } from './utils'
                 createSuggestionBox(chatSection)
 
             const spans = document.querySelectorAll('span.copyable-text')
-            spans.forEach((span) => {
-                if (!span.onclick) {
-                    span.onclick = async () => {
-                        const suggestions = []
-                        try {
-                            // const response = await fetch(
-                            //     'http://localhost:5000/v1/chat',
-                            //     {
-                            //         method: 'post',
-                            //         body: JSON.stringify({
-                            //             userMessage: span.innerText,
-                            //         }),
-                            //         headers: {
-                            //             "Content-type": "application/json"
-                            //         }
-                            //     }
-                            // )
 
-                            // const result = await response.json()
-                            // result.choices.forEach((choice) => suggestions.push(choice.message.content))
-                            suggestions.push(span.innerText)
-                            suggestions.push(isEnabled)
-                            suggestions.push(temperature)
-                            suggestions.push(nSuggestions)
-                            suggestions.push(topP)
+            if (isEnabled)
+                spans.forEach((span) => {
+                    if (!span.onclick) {
+                        span.onclick = async () => {
+                            const suggestions = []
+                            const key = transformText(span.innerText)
+                            try {
+                                createSpinner()
+                                const localResponse =
+                                    await chrome.storage.local.get([key])
+                                let responseValue
 
-                            showSuggestions(suggestions)
-                        } catch (err) {
-                            showSuggestions([err])
+                                for(let respKey in localResponse) {
+                                    responseValue = localResponse[respKey]
+                                }
+
+                                if (responseValue) {
+                                    const choices = JSON.parse(
+                                        responseValue
+                                    )
+                                    choices.forEach((choice) =>
+                                        suggestions.push(choice)
+                                    )
+                                } else {
+                                    const response = await fetch(
+                                        'http://localhost:5000/v1/chat',
+                                        {
+                                            method: 'post',
+                                            body: JSON.stringify({
+                                                userMessage: span.innerText,
+                                                temperature: temperature,
+                                                topP: topP,
+                                                n: nSuggestions,
+                                            }),
+                                            headers: {
+                                                'Content-type':
+                                                    'application/json',
+                                            },
+                                        }
+                                    )
+                                    const result = await response.json()
+                                    console.log(result)
+                                    result.choices && result.choices.forEach((choice) =>
+                                        suggestions.push(choice.message.content)
+                                    )
+
+                                    await chrome.storage.local.set({
+                                        [key]: JSON.stringify(suggestions),
+                                    })
+                                }
+
+                                removeSpinner()
+                                showSuggestions(suggestions)
+                            } catch (err) {
+                                showSuggestions([err])
+                            }
                         }
+                        span.style.cursor = 'pointer'
                     }
-                    span.style.cursor = 'pointer'
-                }
-            })
+                })
+            else
+                spans.forEach((span) => {
+                    span.onclick = null
+                    span.style.cursor = 'text'
+                    document.getElementById(
+                        'suggestion-box-show-button'
+                    ).style.display = 'none'
+                    document.getElementById('suggestion-box').style.display =
+                        'none'
+                })
         }
     }, 1000)
-})()
+}
+
+mainScript()
--- plugin/src/utils/utils.js
+++ plugin/src/utils/utils.js
@@ -6,6 +6,31 @@ export const getActiveTab = async () => {
     return tab
 }
 
+export const createSpinner = () => {
+    const suggestionBox = document.getElementById('suggestion-box')
+    const showButton = document.getElementById('suggestion-box-show-button')
+    const spinnerContainer = document.createElement('div')
+    const spinner = document.createElement('div')
+
+    suggestionBox.replaceChildren()
+    suggestionBox.appendChild(createSuggestionBoxHeader())
+
+    spinnerContainer.classList.add('smart-spinner-container')
+    spinner.classList.add('smart-spinner')
+    suggestionBox.style.display = 'flex'
+    showButton.style.display = 'none'
+
+    spinnerContainer.appendChild(spinner)
+    suggestionBox.appendChild(spinnerContainer)
+}
+
+export const removeSpinner = () => {
+    console.log('calling remove spinner')
+    const suggestionBox = document.getElementById('suggestion-box')
+    const spinnerContainer = document.querySelector('.smart-spinner-container')
+    suggestionBox.removeChild(spinnerContainer)
+}
+
 const createSuggestionBoxHeader = () => {
     const suggestionBoxHeader = document.createElement('section')
     const heading = document.createElement('span')
@@ -20,7 +45,7 @@ const createSuggestionBoxHeader = () => {
 
     hideButton.onclick = () => {
         suggestionBoxHeader.parentElement.style.display = 'none'
-        showButton.style.display = 'block'
+        showButton.style.display = 'flex'
     }
 
     suggestionBoxHeader.appendChild(heading)
@@ -32,17 +57,20 @@ const createSuggestionBoxHeader = () => {
 export const createSuggestionBox = (DOMElement) => {
     const suggestionBox = document.createElement('div')
     const showButton = document.createElement('button')
+    const icon = document.createElement('img')
 
     suggestionBox.id = 'suggestion-box'
     showButton.id = 'suggestion-box-show-button'
+    icon.alt = 'AI'
+    icon.src = chrome.runtime.getURL('assets/icon32.png')
 
-    showButton.innerText = 'AI'
-
+    
     showButton.onclick = () => {
         showButton.style.display = 'none'
         suggestionBox.style.display = 'flex'
     }
-
+    
+    showButton.appendChild(icon)
     suggestionBox.appendChild(createSuggestionBoxHeader())
     DOMElement.appendChild(suggestionBox)
     DOMElement.appendChild(showButton)
@@ -50,6 +78,7 @@ export const createSuggestionBox = (DOMElement) => {
 
 export const showSuggestions = (suggestions) => {
     const suggestionBox = document.getElementById('suggestion-box')
+    const showButton = document.getElementById('suggestion-box-show-button')
     suggestionBox.replaceChildren()
     suggestionBox.appendChild(createSuggestionBoxHeader())
 
@@ -59,19 +88,22 @@ export const showSuggestions = (suggestions) => {
         p.onclick = async () => {
             await navigator.clipboard.writeText(p.innerText)
             suggestionBox.style.display = 'none'
-
+            showButton.style.display = 'flex'
+            suggestionBox
             const toast = document.createElement('div')
             toast.id = 'suggestion-box-toast'
-            toast.innerText = 'Successfully copied the suggestion to the clipboard'
+            toast.innerText =
+                'Successfully copied the suggestion to the clipboard'
             suggestionBox.parentElement.appendChild(toast)
 
             setTimeout(() => {
                 suggestionBox.parentElement.removeChild(toast)
-            }, 4000)
+            }, 2500)
         }
         suggestionBox.appendChild(p)
     })
-    suggestionBox.style.display = 'flex'
+    // suggestionBox.style.display = 'flex'
+    // showButton.style.display = 'none'
 }
 
 export const getConfiguration = async () => {
@@ -86,4 +118,17 @@ export const getConfiguration = async () => {
         nSuggestions: nSuggestions.suggestions,
         topP: topP.topP,
     }
+}
+
+export const transformText = (text) => {
+    const punctuations = /[?.,\/#!$%\^&\*;:{}=\-_`~()]/
+    const textArray = [...text]
+    let transformedText = ''
+
+    textArray.forEach(char => {
+        if(!punctuations.test(char))
+            transformedText += char
+    })
+
+    return transformedText.toLowerCase().replace(/ +/g, ' ')
 }
